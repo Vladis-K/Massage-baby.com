856605b77c48b7985b9c2eb6813a94d15ffb5dbc
Vladys <vladis.kh@gmail.com>
2016-12-19 20:44:32 +0200

    fixed_some_bugs_and_improvements

--- app/js/main.js
+++ app/js/main.js
@@ -1,44 +1,42 @@
 $(document).ready(function(){
 
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+    
+    // Animation of top phrase
+    
 
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  });
-})(jQuery);
-
-
-// Animation of top phrase
-$("#animation-1").addClass('animated zoomIn');
-
-$("#menu").on("click","a", function (event) {
-    event.preventDefault();
-    var id  = $(this).attr('href'),
-      top = $(id).offset().top;
-    $('body,html').animate({
-      scrollTop: top
-    }, 2000);
-  });
+        $("#menu").on("click","a", function (event) {
+            event.preventDefault();
+            var id  = $(this).attr('href'),
+              top = $(id).offset().top;
+            $('body, html').animate({
+              scrollTop: top
+            }, 2000);
+          });
 
 // Smooth scroll
-$("#back-top").hide();
-   $(function () {
-   $(window).scroll(function () {
-   if ($(this).scrollTop() > 100) {
-   $('#back-top').fadeIn();
-   } else {
-   $('#back-top').fadeOut();
-   }
-   });
-   $('#back-top a').click(function (){
-   $('body,html').animate({
-   scrollTop: 0
-   }, 1400);
-   return false;
-});
-});
+        $("#back-top").hide();
+           $(function () {
+           $(window).scroll(function () {
+           if ($(this).scrollTop() > 100) {
+           $('#back-top').fadeIn();
+           } else {
+           $('#back-top').fadeOut();
+           }
+           });
+           $('#back-top a').click(function (){
+           $('body,html').animate({
+           scrollTop: 0
+           }, 1400);
+           return false;
+        });
+    });
 
 });
+
+document.addEventListener('DOMContentLoaded', mainSign, false);
+function mainSign() {
+    var appear = document.getElementById("mainSign");
+    appear.className += " animated zoomIn";
+};
